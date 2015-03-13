7d797784e6d518718b7fbf7c2f2fe2b21e41f631
Leticia Lozano <leloor@gmail.com>
2015-03-12 22:47:35 -0700

    Add GET for api/feed

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -4,5 +4,10 @@ var router = express.Router();
 router.route('/feed')
     .post(function(req,res){
         console.log(req);
+        res.json({message: 'POST received'});
+    })
+    .get(function (req,res) {
+        console.log(req);
+        res.json({message: 'GET received'});
     });
 module.exports=router;
