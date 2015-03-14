0adeb34b99dfb940a495b7390624bd10e8d77fa9
Leticia Lozano <leloor@gmail.com>
2015-03-13 22:26:16 -0700

    Subscribe to instagram tags

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var url = require('url');
 var router = express.Router();
 
 router.route('/feed')
@@ -7,7 +8,22 @@ router.route('/feed')
         res.json({message: 'POST received'});
     })
     .get(function (req,res) {
-        console.log(req);
-        res.json({message: 'GET received'});
+        var parsedRequest = url.parse(req.url, true),
+            responseBody,
+            responseHeaders;
+
+        responseBody = parsedRequest['query']['hub.challenge'];
+        responseHeaders = {
+            'Content-Length': responseBody.length,
+            'Content-Type': 'text/plain'
+        };
+
+        console.log(responseBody);
+
+        res.writeHead(200, responseHeaders);
+        res.write(responseBody);
+
+        return res.end();
     });
+
 module.exports=router;
--- server.js
+++ server.js
@@ -6,10 +6,10 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-
+var localtunnel = require('localtunnel');
 var app = express();
-
 var feed = require('./routes/api/routes');
+var Instagram = require('./lib/instagram/instagram');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -18,10 +18,6 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', feed);
 
-// development only
-//if ('development' == app.get('env')) {
-//  app.use(express.errorHandler());
-//}
 
 // App routing
 require('./routes/index')(app);
@@ -29,5 +25,15 @@ require('./routes/index')(app);
 // API routing
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
+
+    var tunnel = localtunnel(app.get('port'), { subdomain: 'normandyxo' }, function(err, tunnel) {
+
+        var i = Instagram('1671f69c58f540f5a88828f18a0a1f65', '153be3e243ae464c902d8d710ea7b5f7', tunnel.url + '/api/feed/');
+        i.initializeSubscriptions(['shibainu', 'waffles', 'corgi']);
+    });
+
+    tunnel.on('close', function() {
+        // tunnels are closed
+    });
 });
