d6a51927a5e1f7ee0283dbba72c5a00057f5aaef
Leticia Lozano <leloor@gmail.com>
2015-03-15 13:47:07 -0700

    Remove jade as view engine

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ var app = express(),
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', feed);
 
